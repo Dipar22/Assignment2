93453cb1e10718868bb0aebbe10d7ed9cd38c3ba
Dipar <mdipar22@tbc.edu.np>
2022-10-22 20:59:41 +0630

    my third commit

---
+++ test3.py
@@ -0,0 +1,9 @@
+print("Enter a value for a")
+a=float(input())
+print("Enter a value for b")
+b=float(input())
+print("The value \'a\' was", a ,"and the value \'b\' was",b)
+sum=a+b
+print("The sum of \'a\' and \'b\' is", sum)
+multiply=a*b
+print("The product of \'a\' and \'b\' is",multiply)
